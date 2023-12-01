b2cae975b0c912e062b44e018e10fbd2f3c79475
DineshGowthamK <dineshgowtham33@gmail.com>
2023-12-01 23:01:05 +0530

    Comments Updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//! Promise task 3 is about fetch the data from the Emoji api ("https://restcountries.com/v3.1/all") and
+//! Promise task 3 is about fetch the data from the Rest Countries api ("https://restcountries.com/v3.1/all") and
 //! displaying Country Name, Region, Population, Capital, Flag,Image by using Promise and Fetch.
 
 //! The div tag is slected by getElementsByClassName
